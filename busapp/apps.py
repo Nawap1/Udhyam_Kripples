1f6080b68efa5ab13738a7f96104db25065f7ca2
Anjila-26 <54539708+Anjila-26@users.noreply.github.com>
2023-09-16 22:27:13 +0545

    Delete busapp directory

--- busapp/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class BusappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'busapp'
-
-    def ready(self):
-        import busapp.signals
